0e7b539f7e5de6d85e15941b4436279c31bb0d21
Nikita Vereschagin <vereshagin21122006nikita@gmail.com>
2023-01-27 18:25:09 +0700

    final

--- scripts/script.js
+++ scripts/script.js
@@ -5,7 +5,7 @@
 const popupEditProfileBox = document.querySelector('#popupEditProfileBox')
 const popupEditProfile = popupEditProfileBox.querySelector('.popup__container');
 const userName = popupEditProfile.querySelector('#nameInput');
-const userDiscription = popupEditProfile.querySelector('#selectorInput');
+const userDiscription = popupEditProfile.querySelector('#descriptionInput');
 const popupEditProfileCloseIcon = popupEditProfile.querySelector('.popup__close-icon');
 const popupEditProfileSubmit = popupEditProfile.querySelector('.popup__submit-button');
 const popupEditProfileButton = document.querySelector('#buttonEditProfilePopup');
@@ -17,7 +17,7 @@ const userDiscriptionNow = document.querySelector('.profile__user-description');
 const popupAddCardBox = document.querySelector('#popupAddCardBox')
 const popupAddCard = popupAddCardBox.querySelector('.popup__container');
 const cardName = popupAddCard.querySelector('#nameInput');
-const cardImage = popupAddCard.querySelector('#selectorInput');
+const cardImage = popupAddCard.querySelector('#linkInput');
 const popupAddCardCloseIcon = popupAddCard.querySelector('.popup__close-icon');
 const popupAddCardSubmit = popupAddCard.querySelector('.popup__submit-button');
 const popupAddCardButton = document.querySelector('#buttonAddCardPopup');
@@ -71,20 +71,31 @@ popupEditProfile.addEventListener('submit', editProfilePopup)
 
 //функционал карточки
 
-function createCard(card) {
-  card.querySelector('.element__like').addEventListener('click', (evt) => {
+newCard = {link: cardImage.value, name: cardName.value}
+
+function createCard(el) {
+  const element = elTemplate.querySelector('.element').cloneNode(true);
+  const elementImage = element.querySelector('.element__image');
+  const elementCaption = element.querySelector('.element__caption');
+
+  elementImage.src = el.link;
+  elementCaption.textContent = el.name;
+  elementImage.alt = el.textContent;
+
+  element.querySelector('.element__like').addEventListener('click', (evt) => {
     evt.target.classList.toggle('element__like_status_active');
   });
 
-  card.querySelector('.element__trash').addEventListener('click', () => { card.remove() });
+  element.querySelector('.element__trash').addEventListener('click', () => { element.remove() });
 
-  card.querySelector('.element__image').addEventListener('click', () => { 
+  element.querySelector('.element__image').addEventListener('click', () => { 
     openPopup(popupOpenImageBox);
-    imageSrc.src = card.querySelector('.element__image').src;
-    imageName.textContent = card.querySelector('.element__caption').textContent; 
+    imageSrc.src = element.querySelector('.element__image').src;
+    imageName.textContent = element.querySelector('.element__caption').textContent; 
     imageSrc.alt = imageName.textContent
   });
   
+  elContainer.prepend(element);
 }
 
  //добавление карточек из массива выше
@@ -93,31 +104,18 @@ const elTemplate = document.querySelector('#template').content;
 const elContainer = document.querySelector('.elements__container');
 
 initialCards.forEach(function(el) {
-  const element = elTemplate.querySelector('.element').cloneNode(true);
-  element.querySelector('.element__image').src = el.link;
-  element.querySelector('.element__caption').textContent = el.name;
-  element.querySelector('.element__image').alt = el.name;
-
-  createCard(element);
-
-  elContainer.prepend(element);
+  createCard(el);
 });
 
 // Добавление карточек попапом
 
 function addCard(evt) {
   evt.preventDefault();
-  const element = elTemplate.querySelector('.element').cloneNode(true);
-  element.querySelector('.element__image').src = cardImage.value;
-  element.querySelector('.element__caption').textContent = cardName.value;
-  element.querySelector('.element__image').alt = cardName.value;
 
-  createCard(element);
-
+  newCard = {link: cardImage.value, name: cardName.value}
+  createCard(newCard)
+  
   closePopup(popupAddCardBox);
-
-  elContainer.prepend(element);
 }
 
 popupAddCard.addEventListener('submit', addCard);
-// Обсолютно не понимаю, как сделать так, чтобы строчки 97-98 и 111-112 работали вместе. Они разные, но выполняют одну фукцию. Я попробовал, работает лишь добавление карточек. А вот про карточки из массива, он просто создает 12 пустых карточек.
